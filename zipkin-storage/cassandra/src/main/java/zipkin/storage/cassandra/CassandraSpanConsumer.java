b23e40d3f3c82ec9ad46c9cdb12a8a53a1154451
zhaoqi <zhaoqi@ihomefnt.com>
2017-11-08 14:25:46 +0800

    存储阶段记录父子信息

--- zipkin-storage/cassandra/src/main/java/zipkin/storage/cassandra/CassandraSpanConsumer.java
+++ zipkin-storage/cassandra/src/main/java/zipkin/storage/cassandra/CassandraSpanConsumer.java
@@ -14,7 +14,6 @@
 package zipkin.storage.cassandra;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.annotation.JSONField;
 import com.datastax.driver.core.*;
 import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
@@ -24,23 +23,25 @@ import com.google.common.base.Functions;
 import com.google.common.cache.CacheBuilderSpec;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.ihomefnt.common.simhash.core.DefaultSimHashCalculator;
-import com.ihomefnt.common.simhash.core.SimHashCalculator;
-import com.ihomefnt.common.simhash.fingerprint.DefaultMd5FingerPrintCalculator;
-import com.ihomefnt.common.simhash.tokenizer.DefaultSimHashTokenizer;
-import org.apache.commons.lang.StringUtils;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import zipkin.BinaryAnnotation;
 import zipkin.Codec;
 import zipkin.Span;
@@ -54,7 +55,6 @@ import static zipkin.internal.ApplyTimestampAndDuration.guessTimestamp;
 import static zipkin.storage.cassandra.CassandraUtil.bindWithName;
 
 final class CassandraSpanConsumer implements GuavaSpanConsumer {
-  private static final Executor executor = Executors.newCachedThreadPool();
   private static final Logger LOG = LoggerFactory.getLogger(CassandraSpanConsumer.class);
   private static final long WRITTEN_NAMES_TTL
       = Long.getLong("zipkin.store.cassandra.internal.writtenNamesTtl", 60 * 60 * 1000);
@@ -70,8 +70,7 @@ final class CassandraSpanConsumer implements GuavaSpanConsumer {
   private final PreparedStatement insertSpan;
   private final PreparedStatement insertServiceName;
   private final PreparedStatement insertSpanName;
-  private final PreparedStatement insertServiceTreeRoot;
-  private final PreparedStatement insertServiceTree;
+  private final PreparedStatement insertServiceTreeNode;
   private final Schema.Metadata metadata;
   private final DeduplicatingExecutor deduplicatingExecutor;
   private final CompositeIndexer indexer;
@@ -104,19 +103,13 @@ final class CassandraSpanConsumer implements GuavaSpanConsumer {
             .value("bucket", 0) // bucket is deprecated on this index
             .value("span_name", QueryBuilder.bindMarker("span_name"))));
 
-    insertServiceTreeRoot = session.prepare(
+    insertServiceTreeNode = session.prepare(
       maybeUseTtl(QueryBuilder
-        .insertInto("root_tree")
-        .value("key",QueryBuilder.bindMarker("key"))
-        .value("root_service", QueryBuilder.bindMarker("root_service"))
-        .value("service_tree", QueryBuilder.bindMarker("service_tree"))));
-
-    insertServiceTree = session.prepare(
-      maybeUseTtl(QueryBuilder
-        .insertInto("service_call")
-        .value("service_name",QueryBuilder.bindMarker("service_name"))
-        .value("parent_service", QueryBuilder.bindMarker("parent_service"))
-        .value("root_service", QueryBuilder.bindMarker("root_service"))));
+        .insertInto("zeus_service_node")
+        .value("service_name",QueryBuilder.bindMarker("serviceName"))
+        .value("parent_service", QueryBuilder.bindMarker("parentService"))
+        .value("root_service", QueryBuilder.bindMarker("rootService"))
+        .value("tag", QueryBuilder.bindMarker("tag"))));
 
     deduplicatingExecutor = new DeduplicatingExecutor(session, WRITTEN_NAMES_TTL);
     indexer = new CompositeIndexer(session, indexCacheSpec, bucketCount, this.indexTtl);
@@ -134,10 +127,7 @@ final class CassandraSpanConsumer implements GuavaSpanConsumer {
    */
   @Override
   public ListenableFuture<Void> accept(List<Span> rawSpans) {
-    /**
-     * 暂时不存这部分数据
-     */
-//    storeServiceCallTree(rawSpans);
+    storeServiceTree(rawSpans);
     ImmutableSet.Builder<ListenableFuture<?>> futures = ImmutableSet.builder();
 
     ImmutableList.Builder<Span> spans = ImmutableList.builder();
@@ -168,210 +158,6 @@ final class CassandraSpanConsumer implements GuavaSpanConsumer {
     }
     futures.addAll(indexer.index(spans.build()));
     return transform(Futures.allAsList(futures.build()), TO_VOID);
-  }
-
-  /**
-   * #####################################
-   */
-  private void storeServiceCallTree(List<Span> sampled) {
-    List<Span> spanList = new ArrayList<>(sampled);
-    executor.execute(new Runnable() {
-      @Override
-      public void run() {
-        ServiceCallTree root = buildServiceTree(spanList);
-        if (null != root) {
-          storeCallTreeRoot(root);
-          storeCallTreeNode(root.getChildren(), root, root);
-        }
-      }
-
-      private void storeCallTreeNode(List<ServiceCallTree> children, ServiceCallTree parent, ServiceCallTree root) {
-        if (null == children || children.size() <= 0) {
-          return;
-        }
-        // store service relation
-        for (ServiceCallTree child : children) {
-          BoundStatement bound = CassandraUtil.bindWithName(insertServiceTree, "insert-tree-node")
-            .setString("service_name", child.getServiceName())
-            .setString("parent_service", parent.getServiceName())
-            .setString("root_service", root.getServiceName());
-          if (!metadata.hasDefaultTtl) bound.setInt("ttl_", spanTtl);
-          deduplicatingExecutor.maybeExecuteAsync(bound, child.getServiceName() + "."
-            + parent.getServiceName() + "." + root.getServiceName());
-          // 递归存储子节点
-          storeCallTreeNode(child.getChildren(), child, root);
-        }
-      }
-
-      /**
-       * @param root
-       */
-      private void storeCallTreeRoot(ServiceCallTree root) {
-        SimHashCalculator calculator = DefaultSimHashCalculator.DefaultSimHashCalculatorBuilder.aDefaultSimHashCalculator()
-          .fingerPrintCalculator(new DefaultMd5FingerPrintCalculator())
-          .simHashTokenizer(new DefaultSimHashTokenizer())
-          .build();
-
-        String serviceTreeJson = JSON.toJSONString(root);
-        String key = calculator.getSimHash(serviceTreeJson);
-        // store root
-        BoundStatement bound = CassandraUtil.bindWithName(insertServiceTreeRoot, "insert-tree-root")
-          .setString("key", key)
-          .setString("root_service", root.getServiceName())
-          .setString("service_tree", serviceTreeJson);
-
-        BoundStatement nodeBound = CassandraUtil.bindWithName(insertServiceTree, "insert-tree-node")
-          .setString("service_name", root.getServiceName())
-          .setString("parent_service", "")
-          .setString("root_service", "");
-        if (!metadata.hasDefaultTtl) bound.setInt("ttl_", spanTtl);
-        if (StringUtils.isNotEmpty(key)) deduplicatingExecutor.maybeExecuteAsync(bound, key);
-        if (StringUtils.isNotEmpty(root.getServiceName())) deduplicatingExecutor.maybeExecuteAsync(nodeBound, root.getServiceName());
-      }
-    });
-  }
-
-  private ServiceCallTree buildServiceTree(List<Span> sampled) {
-    Span rootSpan = null;
-    for (Span span : sampled) {
-      if (span.parentId == null) {
-        rootSpan =span;
-        break;
-      }
-    }
-    if (null != rootSpan) {
-      ServiceCallTree root = new ServiceCallTree();
-      root.setServiceName(buildServiceName(rootSpan));
-      root.setSpanId(rootSpan.id);
-      sampled.remove(rootSpan);
-      buildChildren(root, sampled);
-      return root;
-    }
-
-    return null;
-  }
-
-  private String buildServiceName(Span span) {
-    // 优先从stackTrace中取serviceName
-    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
-      if (binaryAnnotation.key.equals("stackTrace")) {
-        StackTrace stackTrace = JSON.parseObject(new String(binaryAnnotation.value, Charset.forName("UTF-8")), StackTrace.class);
-        if (null != stackTrace.getServiceName() && stackTrace.getServiceName().length() > 0) {
-          return stackTrace.getServiceName();
-        }
-      }
-    }
-
-    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
-      if ("http.url".equals(binaryAnnotation.key)) {
-        String value = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
-        Pattern urlPathVariablePattern = Pattern.compile(".*/[0-9]+/.*");
-        Matcher matcher = urlPathVariablePattern.matcher(value);
-        if (matcher.matches()) {
-          value = value.split("/[0-9]+/")[0];
-        }
-        if (!value.startsWith("http://")) {
-          value = value.replace("/",".");
-          if (value.startsWith(".")) {
-            return value.substring(value.indexOf(".") + 1);
-          }
-          return value;
-        }
-      }
-    }
-
-    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
-      if ("http.url".equals(binaryAnnotation.key)) {
-        String value = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
-        Pattern urlPathVariablePattern = Pattern.compile(".*/[0-9]+/.*");
-        Matcher matcher = urlPathVariablePattern.matcher(value);
-        if (matcher.matches()) {
-          value = value.split("/[0-9]+/")[0];
-        }
-        value = value.replaceAll("http://[^/]+/","");
-        return value.replace("/",".");
-      }
-    }
-    return null;
-  }
-
-
-  private void buildChildren(ServiceCallTree serviceCallTree, List<Span> spans) {
-    if (spans == null || spans.size() == 0) {
-      return ;
-    }
-    List<Span> removeList = new ArrayList<>();
-    for (Span span : spans) {
-      if (null != span.parentId && span.parentId.equals(serviceCallTree.getSpanId())) {
-        serviceCallTree.addChild(span);
-        removeList.add(span);
-      }
-    }
-
-    if (removeList.size()==0) {
-      // 已到叶子节点
-      serviceCallTree.setLeaf(true);
-      return ;
-    }
-    // 去除已经构造过的span
-    spans.removeAll(removeList);
-
-    // 递归生成树
-    for (ServiceCallTree tree : serviceCallTree.getChildren()) {
-      buildChildren(tree, spans);
-    }
-
-  }
-
-
-   class ServiceCallTree {
-
-    @JSONField(serialize = false,deserialize = false)
-    private Long spanId;
-
-    private String serviceName;
-
-    private boolean leaf;
-
-    private List<ServiceCallTree> children = new ArrayList<>();
-
-    public String getServiceName() {
-      return serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-      this.serviceName = serviceName;
-    }
-
-    public List<ServiceCallTree> getChildren() {
-      return children;
-    }
-
-    public Long getSpanId() {
-      return spanId;
-    }
-
-    public void setSpanId(Long spanId) {
-      this.spanId = spanId;
-    }
-
-    public void addChild(Span span) {
-      String serviceName = buildServiceName(span);
-      if (StringUtils.isNotEmpty(serviceName)) {
-        ServiceCallTree child = new ServiceCallTree();
-        child.setSpanId(span.id);
-        child.setServiceName(serviceName);
-        children.add(child);
-      }
-    }
-
-    public boolean isLeaf() {
-      return leaf;
-    }
-
-    public void setLeaf(boolean leaf) {
-      this.leaf = leaf;
-    }
   }
 
   /**
@@ -418,5 +204,236 @@ final class CassandraSpanConsumer implements GuavaSpanConsumer {
   @VisibleForTesting void clear() {
     indexer.clear();
     deduplicatingExecutor.clear();
+  }
+
+  /**
+   * @Author onefish
+   * add Zeus Plus
+   */
+
+  private final static Integer ZEUS_TTL = 604800000;
+
+  private void storeServiceTree(List<Span> spans) {
+    if (CollectionUtils.isEmpty(spans)) {
+      return ;
+    }
+    Map<Long,Span> spanMap = new HashMap<>();
+    Set<Long> parentIdSet = Sets.newHashSet();
+    for (Span span : spans) {
+      spanMap.put(span.id,span);
+      if (null != span.parentId) {
+        if (!isSqlQuery(span))
+          parentIdSet.add(span.parentId);
+      }
+    }
+    Long traceId = spans.get(0).traceId;
+    String rootService = "";
+    if (spanMap.containsKey(traceId)) {
+      Span rootSpan = spanMap.get(traceId);
+      rootService = guessZeusServiceName(rootSpan);
+    }
+    Set<String> existHelper = Sets.newHashSet();
+    for (Span span : spans) {
+      String serviceName = guessZeusServiceName(span);
+      if (StringUtils.isEmpty(serviceName)) {
+        continue;
+      }
+      String parentServiceName = guessZeusServiceName(spanMap.get(span.parentId));
+      if (!existHelper.contains(serviceName+"->"+parentServiceName)) {
+        ZeusServiceTreeNode treeNode = new ZeusServiceTreeNode();
+        treeNode.serviceName = serviceName;
+        treeNode.parentService = parentServiceName;
+        if (!StringUtils.isEmpty(rootService)) {
+          treeNode.rootService = rootService;
+        }
+        storeTreeNode(treeNode);
+        existHelper.add(serviceName+"->"+parentServiceName);
+      }
+    }
+  }
+
+  private boolean isSqlQuery(Span span) {
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if (binaryAnnotation.key.equals("sql.query")) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+
+  /**
+   * 当不是所有应用升级到zeus2.1 会导致rootService不准确
+   * @param span
+   * @return
+   */
+  private String guessRootService(Span span) {
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if ("servicePath".equals(binaryAnnotation.key) && null != binaryAnnotation.value) {
+        String servicePath = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
+        if (!StringUtils.isEmpty(servicePath)) {
+          // 优先从header中获取服务名
+          String rootService;
+          if (servicePath.contains("#")) {
+            rootService = servicePath.substring(0,servicePath.indexOf("#"));
+          } else {
+            rootService = servicePath;
+          }
+          if (!StringUtils.isEmpty(rootService)) {
+            return rootService;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  private String guessZeusServiceName(Span span) {
+    if (null == span) {
+      return "";
+    }
+    // 优先取serviceName
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if ("servicePath".equals(binaryAnnotation.key) && null != binaryAnnotation.value) {
+        String servicePath = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
+        if (!StringUtils.isEmpty(servicePath)) {
+          // 优先从header中获取服务名
+          String serviceName;
+          if (servicePath.contains("#")) {
+            serviceName = servicePath.substring(servicePath.lastIndexOf("#"));
+          } else {
+            serviceName = servicePath;
+          }
+          if (!StringUtils.isEmpty(serviceName)) {
+            return serviceName;
+          }
+        }
+      }
+    }
+
+    // 然后取stackTrace中的ServiceName
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if (binaryAnnotation.key.equals("stackTrace")) {
+        StackTrace stackTrace = JSON.parseObject(new String(binaryAnnotation.value, Charset.forName("UTF-8")), StackTrace.class);
+        if (null != stackTrace.getServiceName() && stackTrace.getServiceName().length() > 0) {
+          return stackTrace.getServiceName();
+        }
+      }
+    }
+    // 全部升级到2.1后 为了保证服务名的准确性，根span将不从url中取
+    String serviceGroup = span.annotations.get(0).endpoint.serviceName;
+    String serviceName = getFromUrl(span);
+    if (!org.springframework.util.StringUtils.isEmpty(serviceName) && !serviceName.startsWith(serviceGroup)) {
+      serviceName = serviceGroup + "." + serviceName;
+    }
+    return serviceName;
+  }
+
+  /**
+   * 根据url获取服务名
+   * @param span
+   * @return
+   */
+  private String getFromUrl(Span span) {
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if ("http.url".equals(binaryAnnotation.key)) {
+        String value = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
+        Pattern urlPathVariablePattern = Pattern.compile(".*/[0-9]+/.*");
+        Matcher matcher = urlPathVariablePattern.matcher(value);
+        if (matcher.matches()) {
+          value = value.split("/[0-9]+/")[0];
+        }
+        if (!value.startsWith("http://")) {
+          value = value.replace("/",".");
+          if (value.startsWith(".")) {
+            return value.substring(value.indexOf(".") + 1);
+          }
+          return value;
+        }
+      }
+    }
+
+    for (BinaryAnnotation binaryAnnotation : span.binaryAnnotations) {
+      if ("http.url".equals(binaryAnnotation.key)) {
+        String value = new String(binaryAnnotation.value, Charset.forName("UTF-8"));
+        Pattern urlPathVariablePattern = Pattern.compile(".*/[0-9]+/.*");
+        Matcher matcher = urlPathVariablePattern.matcher(value);
+        if (matcher.matches()) {
+          value = value.split("/[0-9]+/")[0];
+        }
+        value = value.replaceAll("http://[^/]+/","");
+        return value.replace("/",".");
+      }
+    }
+    return "";
+  }
+
+
+  private void storeTreeNode(ZeusServiceTreeNode zeusServiceTreeNode) {
+    BoundStatement bound = CassandraUtil.bindWithName(insertServiceTreeNode, "insert-tree-node")
+      .setString("serviceName", zeusServiceTreeNode.getServiceName())
+      .setString("parentService", zeusServiceTreeNode.getParentService())
+      .setString("rootService", zeusServiceTreeNode.getRootService())
+      .setString("tag", zeusServiceTreeNode.getTreeTag());
+    setTtl(bound);
+    deduplicatingExecutor.maybeExecuteAsync(bound, zeusServiceTreeNode.toString());
+  }
+
+  private void setTtl(BoundStatement bound) {
+    bound.setInt("ttl_",ZEUS_TTL);
+  }
+
+  class ZeusServiceTreeNode {
+
+    private String serviceName;
+
+    private String parentService;
+
+    private String rootService;
+
+    private String treeTag;
+
+    public String getServiceName() {
+      return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+      this.serviceName = serviceName;
+    }
+
+    public String getParentService() {
+      return parentService;
+    }
+
+    public void setParentService(String parentService) {
+      this.parentService = parentService;
+    }
+
+    public String getRootService() {
+      return rootService;
+    }
+
+    public void setRootService(String rootService) {
+      this.rootService = rootService;
+    }
+
+    public String getTreeTag() {
+      return treeTag;
+    }
+
+    public void setTreeTag(String treeTag) {
+      this.treeTag = treeTag;
+    }
+
+    @Override
+    public String toString() {
+      return "ZeusServiceTreeNode{" +
+        "serviceName='" + serviceName + '\'' +
+        ", parentService='" + parentService + '\'' +
+        ", rootService='" + rootService + '\'' +
+        ", treeTag='" + treeTag + '\'' +
+        '}';
+    }
   }
 }
